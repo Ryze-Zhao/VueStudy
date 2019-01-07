820fcf9f3ba6ff53b737456dd41db015dd309989
RyzeZhao <643080112@qq.com>
2019-01-07 16:11:58 +0800

    Vue-Basic：manyVue：初始化

---
+++ Vue-1/basic-13-manyVue/my.js
@@ -0,0 +1,23 @@
+//实例化Vue对象
+var one=new Vue({
+    el: "#basic-1",
+    data: {
+       
+    },methods: {
+        
+    },computed: {
+        
+    }
+});
+
+//实例化Vue对象
+var two=    new Vue({
+    el: "#basic-2",
+    data: {
+       
+    },methods: {
+        
+    },computed: {
+        
+    }
+});
