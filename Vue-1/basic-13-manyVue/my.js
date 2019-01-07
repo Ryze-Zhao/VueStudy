76d384dcd8115da2f44c4694252fbd7df6788fca
RyzeZhao <643080112@qq.com>
2019-01-07 16:19:21 +0800

    Vue-Basic：manyVue：更新

--- Vue-1/basic-13-manyVue/my.js
+++ Vue-1/basic-13-manyVue/my.js
@@ -2,11 +2,13 @@
 var one=new Vue({
     el: "#basic-1",
     data: {
-       
+       title:"Vue1"
     },methods: {
         
     },computed: {
-        
+        greet:function(){
+            return "Hello Vue1";
+        }
     }
 });
 
@@ -14,10 +16,15 @@ var one=new Vue({
 var two=    new Vue({
     el: "#basic-2",
     data: {
-       
+    title:"Vue2"
     },methods: {
-        
+        //从另外一个Vue里面改变Vue的属性等
+        changeVue1Title:function(){
+            one.title="Vue1被Vue2改名！"
+        }
     },computed: {
-        
+        greet:function(){
+            return "Hello Vue2";
+        }
     }
 });
