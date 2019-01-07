3391fe44e6960df382c5d3a5a23ebf4c39ef1802
RyzeZhao <643080112@qq.com>
2019-01-07 16:07:41 +0800

    Vue-Basic：Picture：更新

--- Vue-1/basic-12-picture/my.js
+++ Vue-1/basic-12-picture/my.js
@@ -2,10 +2,19 @@
 new Vue({
     el: "#basic-1",
     data: {
-       
-    },methods: {
-        
-    },computed: {
-        
+        health: 100,
+        ended: false
+    }, methods: {
+        punch: function () {
+            this.health -= 10;
+            if (this.health <= 0) {
+                this.ended = true;
+            }
+        }, restart: function () {
+            this.health = 100;
+            this.ended = false;
+        }
+    }, computed: {
+
     }
 });
