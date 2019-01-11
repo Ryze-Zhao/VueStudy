e29d565c7b60e384bd12a56114586f76cfecda4b
RyzeZhao <643080112@qq.com>
2019-01-11 15:00:58 +0800

    Vue-Basic：CssBind：更新

--- Vue-1/basic-9-cssbind/my.js
+++ Vue-1/basic-9-cssbind/my.js
@@ -2,10 +2,17 @@
 new Vue({
     el: "#basic-1",
     data: {
-       dataChangeColor:false,
-       dataChangeLength:false
-    },
-    methods: {
-        
+        dataChangeColor: false,
+        dataChangeLength: false
+    }, methods: {
+
+    }, computed: {
+        myComputed: function () {
+            return {
+                // changeColor是css里面的，dataChangeColor是js里面的data，现在这个意思是如果dataChangeColor为true，显示changeColor样式
+                changeColor:this.dataChangeColor,
+                changeLength:this.dataChangeLength
+            }
+        }
     }
 });
