ebb9ca8ae71bbf5e1555144b41afca49fa06f802
RyzeZhao <643080112@qq.com>
2019-02-03 11:16:29 +0800

    Vue-Basic：Computed：更新

--- Vue-1/basic-8-computed/my.js
+++ Vue-1/basic-8-computed/my.js
@@ -2,9 +2,22 @@
 new Vue({
     el: "#basic-1",
     data: {
-       
-    },
-    methods: {
-        
+        a: 0,
+        b: 0,
+        age: 20
+    }, methods: {
+        // addToA: function () {
+        //     return this.a + this.age;
+        // },
+        // addToB: function () {
+        //     return this.b + this.age;
+        // }
+    }, computed: {
+        addToA: function () {
+            return this.a + this.age;
+        },
+        addToB: function () {
+            return this.b + this.age;
+        }
     }
 });
