8aa91660ca59513a881e287572289460bbc3787e
RyzeZhao <643080112@qq.com>
2019-03-03 22:01:49 +0800

    Vue-Basic：Bind：使用v-bind进行绑定

---
+++ Vue-1/basic-3-bind/my.js
@@ -0,0 +1,25 @@
+//实例化Vue对象
+new Vue({
+    el:"#basic-1",
+    data:{
+        name:"测试1",
+        website:"https://www.hehaozhao.top"
+    },
+    methods:{
+        myFunction:function(){
+            return "测试Methods!";
+        },
+        myFunction1:function(param){
+            return "测试Methods!"+param;
+        },
+        myFunction2:function(param){
+            return "测试Methods!"+param+"   "+this.name;
+        }
+    }
+});
+
+/*
+ * el：选择根容器
+ * data:用于数据的存储
+ * methods：用于存储各种方法
+ */
