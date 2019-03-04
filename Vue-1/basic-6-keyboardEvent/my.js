9c37bd867e6c8858015079bacdd1f8543fd662c9
RyzeZhao <643080112@qq.com>
2019-03-04 09:52:25 +0800

    Vue-Basic：keyboardEvent：初始化

---
+++ Vue-1/basic-6-keyboardEvent/my.js
@@ -0,0 +1,18 @@
+//实例化Vue对象
+new Vue({
+    el: "#basic-1",
+    data: {
+  
+    },
+    methods: {
+     
+    }
+});
+
+/*
+ * el：选择根容器
+ * data:用于数据的存储
+ * methods：用于存储各种方法
+ * v-bind和v-html：给属性绑定对应值
+ * v-on:
+ */
