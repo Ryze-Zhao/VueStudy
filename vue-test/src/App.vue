e037053af23ee6a0af461651994a982e614d1ca1
RyzeZhao <643080112@qq.com>
2019-03-06 15:53:28 +0800

    Vue-Project：Vue-Cli：注册局部组件

--- vue-test/src/App.vue
+++ vue-test/src/App.vue
@@ -6,16 +6,21 @@
 </template>
 
 <script>
+// 这是注册局部组件
+import Users from './components/Users'
+
 export default {
   name: 'app',
   data(){
     return{
       title:"这是脚手架"
     }
+  },components: {
+    // 这是注册局部组件,名字不能与html标签相同
+    "users":Users
   }
 }
 </script>
 
 <style>
-
 </style>
--- vue-test/src/main.js
+++ vue-test/src/main.js
@@ -1,11 +1,12 @@
 import Vue from 'vue'
 import App from './App'
-import Users from './components/Users'
+// 这是注册全局组件
+// import Users from './components/Users'
 
 Vue.config.productionTip = false
 
-//全局注册组件,前面users是要用时的名称，后面Users是上面引入的
-Vue.component("users",Users);
+//全局注册组件,前面users是要用时的名称，后面Users是上面引入的(注意这里component没有s)
+// Vue.component("users",Users);
 
 
 new Vue({
