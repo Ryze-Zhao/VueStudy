f9de5ed69095590336f051eb1346cc5453bbf039
RyzeZhao <643080112@qq.com>
2019-03-06 16:28:01 +0800

    Vue-Project：Vue-Cli：组件Css作用域1

--- vue-test/src/App.vue
+++ vue-test/src/App.vue
@@ -23,4 +23,7 @@ export default {
 </script>
 
 <style>
+h1{
+  color: purple
+}
 </style>
--- vue-test/src/components/Users.vue
+++ vue-test/src/components/Users.vue
@@ -1,6 +1,7 @@
 <template>
 <!-- 通常根据文件名改 -->
   <div class="users">
+    <h1>Users中的H1标签</h1>
     <ul>
       <li v-for="user in users">{{user}}</li>
     </ul>
@@ -20,6 +21,4 @@
 </script>
 
 
-<style scoped>
-
-</style>
+<style></style>
