4a67e0eb0e7a5321fa0bc2566a0a39e6a2b93ea6
RyzeZhao <643080112@qq.com>
2019-03-12 15:34:57 +0800

    Vue-Project：Vue-Cli：实战：初始化

---
+++ vue-test/src/components/Footer.vue
@@ -0,0 +1,22 @@
+<template>
+  <div class="hello">
+    
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'HelloWorld',
+  data () {
+    return {
+     
+    }
+  },components: {
+    
+  }
+}
+</script>
+
+<style scoped>
+
+</style>
