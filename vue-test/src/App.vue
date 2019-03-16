836617724a24cd6b4db869e77134e507e48c88ee
RyzeZhao <643080112@qq.com>
2019-03-16 20:24:20 +0800

    Vue-Cli：属性传值Props

--- vue-test/src/App.vue
+++ vue-test/src/App.vue
@@ -1,7 +1,8 @@
 <template>
   <div id="app">
   <app-header></app-header>
-  <users></users>
+  <!-- 左边的users是传入到Users.vue中，引入的Key；右边的users是下边data的key； -->
+  <users v-bind:users="users"></users>
   <app-footer></app-footer>
   </div>
 </template>
@@ -15,7 +16,16 @@ export default {
   name: 'app',
   data(){
     return{
-      title:"这是脚手架"
+      users: [
+                  {name:"Mario",position:"Web开发",show:false},
+                  {name:"Mario",position:"Web开发",show:false},
+                  {name:"Mario",position:"Web开发",show:false},
+                  {name:"Mario",position:"Web开发",show:false},
+                  {name:"Mario",position:"Web开发",show:false},
+                  {name:"Mario",position:"Web开发",show:false},
+                  {name:"Mario",position:"Web开发",show:false}
+
+              ]
     }
   },components: {
     // 这是注册局部组件,名字不能与html标签相同
--- vue-test/src/components/Users.vue
+++ vue-test/src/components/Users.vue
@@ -15,44 +15,47 @@
   export default {
       // 上边Class名是什么这里跟着
       name: 'users',
-      data() {
-          return {
-              users: [
-                  {name:"Mario",position:"Web开发",show:false},
-                  {name:"Mario",position:"Web开发",show:false},
-                  {name:"Mario",position:"Web开发",show:false},
-                  {name:"Mario",position:"Web开发",show:false},
-                  {name:"Mario",position:"Web开发",show:false},
-                  {name:"Mario",position:"Web开发",show:false},
-                  {name:"Mario",position:"Web开发",show:false}
-
-              ]
+      // 这种方法不太好，下面哪种是官方的建议的
+      // props:["users"],
+      props: {
+          users: {
+              type: Array,
+              required: true
           }
+          // 如果有多个就这么附加
+          // ,hehehehe:{
+
+          // }
+      },
+      data() {
+          return {}
       }
   }
 </script>
 
 
 <style scoped>
-  .users{
+  .users {
       width: 100%;
       max-width: 1200px;
       margin: 40px auto;
       padding: 0 20px;
       box-sizing: border-box;
   }
-  ul{
+
+  ul {
       display: flex;
       flex-wrap: wrap;
       list-style-type: none;
       padding: 0;
   }
-  li{
+
+  li {
       flex-grow: 1;
       flex-basis: 200px;
       text-align: center;
       padding: 30px;
       border: 1px solid #222;
-      margin:10px;
+      margin: 10px;
   }
 </style>
